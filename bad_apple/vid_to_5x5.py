e3bc62fa341364dc7ddcd18e1f9a3abc227647f4
BeyYT <60990459+BeyYT@users.noreply.github.com>
2022-11-01 09:37:38 -0600

    Update vid_to_5x5.py

--- bad_apple/vid_to_5x5.py
+++ bad_apple/vid_to_5x5.py
@@ -1,5 +1,5 @@
 import moviepy.editor as mp
-clip = mp.VideoFileClip("ported_too_much.mp4")
-clip_resized = clip.resize(height=5, width=5) # make the height 360px ( According to moviePy documenation The width is then computed so that the width/height ratio is conserved.)
+clip = mp.VideoFileClip("ported_too_much.mp4") #haha
+clip_resized = clip.resize(height=5, width=5)
 clip_fps = clip_resized.set_fps(30)
 clip_fps.write_videofile("5x5.mp4")
