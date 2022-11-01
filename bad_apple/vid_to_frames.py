005e0e1eaae1ddf1f7a27c25a256bdd0c30b1db3
BeyYT <60990459+BeyYT@users.noreply.github.com>
2022-11-01 09:37:56 -0600

    Update vid_to_frames.py

--- bad_apple/vid_to_frames.py
+++ bad_apple/vid_to_frames.py
@@ -3,7 +3,7 @@ vidcap = cv2. VideoCapture('5x5.mp4')
 success, image = vidcap.read()
 count = 0
 while success:
-    cv2.imwrite(os.path.join(os.getcwd(), "imgs") + "\\%d.png" % count, image) # save frame as JPEG file.
+    cv2.imwrite(os.path.join(os.getcwd(), "imgs") + "\\%d.png" % count, image) # save frame as png file.
     success,image = vidcap.read()
     print('Read a new frame: ', success)
     count = count + 1
