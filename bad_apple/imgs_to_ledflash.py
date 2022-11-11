56dff6c7de3af093faa5c1b54475c7bcc374d3c5
BeyYT <60990459+BeyYT@users.noreply.github.com>
2022-11-10 19:38:57 -0700

    flipped values + space saving update

--- bad_apple/imgs_to_ledflash.py
+++ bad_apple/imgs_to_ledflash.py
@@ -5,7 +5,7 @@ from PIL import Image
 gencode = ""
 
 m = os.listdir("imgs")
-# print(natsorted(m))
+print(natsorted(m))
 
 for i in range(len(m)):
     im = Image.open("imgs\\" + m[i]) # Can be many different formats.
@@ -91,54 +91,55 @@ for i in range(len(m)):
     # print(pixel)
 
     for u in range(len(pixel)):
+        if pixel[u] > 0:
             if u == 0:
                 gencode = gencode + "drawled(0,0,"+str(pixel[u])+");"
             if u == 1:
-                gencode = gencode + "drawled(0,1,"+str(pixel[u])+");"
-            if u == 2:
-                gencode = gencode + "drawled(0,2,"+str(pixel[u])+");"
-            if u == 3:
-                gencode = gencode + "drawled(0,3,"+str(pixel[u])+");"
-            if u == 4:
-                gencode = gencode + "drawled(0,4,"+str(pixel[u])+");"
-            if u == 5:
                 gencode = gencode + "drawled(1,0,"+str(pixel[u])+");"
+            if u == 2:
+                gencode = gencode + "drawled(2,0,"+str(pixel[u])+");"
+            if u == 3:
+                gencode = gencode + "drawled(3,0,"+str(pixel[u])+");"
+            if u == 4:
+                gencode = gencode + "drawled(4,0,"+str(pixel[u])+");"
+            if u == 5:
+                gencode = gencode + "drawled(0,1,"+str(pixel[u])+");"
             if u == 6:
                 gencode = gencode + "drawled(1,1,"+str(pixel[u])+");"
             if u == 7:
-                gencode = gencode + "drawled(1,2,"+str(pixel[u])+");"
-            if u == 8:
-                gencode = gencode + "drawled(1,3,"+str(pixel[u])+");"
-            if u == 9:
-                gencode = gencode + "drawled(1,4,"+str(pixel[u])+");"
-            if u == 10:
-                gencode = gencode + "drawled(2,0,"+str(pixel[u])+");"
-            if u == 11:
                 gencode = gencode + "drawled(2,1,"+str(pixel[u])+");"
+            if u == 8:
+                gencode = gencode + "drawled(3,1,"+str(pixel[u])+");"
+            if u == 9:
+                gencode = gencode + "drawled(4,1,"+str(pixel[u])+");"
+            if u == 10:
+                gencode = gencode + "drawled(0,2,"+str(pixel[u])+");"
+            if u == 11:
+                gencode = gencode + "drawled(1,2,"+str(pixel[u])+");"
             if u == 12:
                 gencode = gencode + "drawled(2,2,"+str(pixel[u])+");"
             if u == 13:
-                gencode = gencode + "drawled(2,3,"+str(pixel[u])+");"
-            if u == 14:
-                gencode = gencode + "drawled(2,4,"+str(pixel[u])+");"
-            if u == 15:
-                gencode = gencode + "drawled(3,0,"+str(pixel[u])+");"
-            if u == 16:
-                gencode = gencode + "drawled(3,1,"+str(pixel[u])+");"
-            if u == 17:
                 gencode = gencode + "drawled(3,2,"+str(pixel[u])+");"
+            if u == 14:
+                gencode = gencode + "drawled(4,2,"+str(pixel[u])+");"
+            if u == 15:
+                gencode = gencode + "drawled(0,3,"+str(pixel[u])+");"
+            if u == 16:
+                gencode = gencode + "drawled(1,3,"+str(pixel[u])+");"
+            if u == 17:
+                gencode = gencode + "drawled(2,3,"+str(pixel[u])+");"
             if u == 18:
                 gencode = gencode + "drawled(3,3,"+str(pixel[u])+");"
             if u == 19:
-                gencode = gencode + "drawled(3,4,"+str(pixel[u])+");"
-            if u == 20:
-                gencode = gencode + "drawled(4,0,"+str(pixel[u])+");"
-            if u == 21:
-                gencode = gencode + "drawled(4,1,"+str(pixel[u])+");"
-            if u == 22:
-                gencode = gencode + "drawled(4,2,"+str(pixel[u])+");"
-            if u == 23:
                 gencode = gencode + "drawled(4,3,"+str(pixel[u])+");"
+            if u == 20:
+                gencode = gencode + "drawled(0,4,"+str(pixel[u])+");"
+            if u == 21:
+                gencode = gencode + "drawled(1,4,"+str(pixel[u])+");"
+            if u == 22:
+                gencode = gencode + "drawled(2,4,"+str(pixel[u])+");"
+            if u == 23:
+                gencode = gencode + "drawled(3,4,"+str(pixel[u])+");"
             if u == 24:
                 gencode = gencode + "drawled(4,4,"+str(pixel[u])+");"
    
